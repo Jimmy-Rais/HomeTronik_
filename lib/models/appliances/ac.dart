edd936c4d4d60a91cdbcf8f995730db8f80e65d5
Jimmy-Rais <raisgachaba@gmail.com>
2024-03-16 07:16:37 +0530

    appliances

---
+++ lib/models/appliances/ac.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+class ac extends StatefulWidget {
+  const Action({super.key});
+
+  @override
+  State<lighhtbulb> createState() => _lighhtbulbState();
+}
+
+class _lighhtbulbState extends State<lighhtbulb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/models/appliances/fan.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class fan extends StatefulWidget {
+  const fan({super.key});
+
+  @override
+  State<fan> createState() => _fanState();
+}
+
+class _fanState extends State<fan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
